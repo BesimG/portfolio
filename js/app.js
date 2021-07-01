4af3dcb014d5d6143085b6fa91f04cb1bf87f0f5
Besim Gjidija <besimgjidija@gmail.com>
2021-07-01 13:24:33 -0400

    Added a margin top and bottom to my light/dark mode button and added a bio to the jumbotron

--- js/app.js
+++ js/app.js
@@ -1,3 +1,26 @@
+const toggle = document.querySelector(".toggle");
+const menu = document.querySelector(".menu");
+ 
+/* Toggle mobile menu */
+function toggleMenu() {
+    if (menu.classList.contains("active")) {
+        menu.classList.remove("active");
+         
+        // adds the menu (hamburger) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
+    } else {
+        menu.classList.add("active");
+         
+        // adds the close (x) icon
+        toggle.querySelector("a").innerHTML = "<i class='fas fa-times'></i>";
+    }
+}
+ 
+/* Event Listener */
+toggle.addEventListener("click", toggleMenu, false);
+
+
+
 // js functionality for dark mode
 document.addEventListener('DOMContentLoaded', () => {
     const themeStylesheet = document.getElementById('theme');
